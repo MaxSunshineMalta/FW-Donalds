23831bf2511a024729a4e63d5885bacfaf17cb13
MaxSunshineMalta <joaocruzmalta12345@gmail.com>
2025-02-18 21:35:17 -0300

    add shadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const PeoductsPage = () => {
+    return (
+        <div>
+            <h1>Products Page</h1>
+            <Button> SDCNWSDC</Button>
+            <Input placeholder="Vamoooo"/>
+        </div>
+    );
+}
+ 
+export default PeoductsPage;
